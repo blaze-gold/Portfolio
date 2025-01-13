e20274371213497fb1a4645fecb5695996b4fdea
David Chen <chenstar@gmail.com>
2025-01-13 03:31:46 -0800

    update:updated module

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -1,7 +1,12 @@
 import { type Metadata } from 'next'
 import { SimpleLayout } from '@/components/layout/SimpleLayout'
 
-import { friendsHeadLine, friendsIntro, projects, githubProjects, friends } from '@/config/infoConfig'
+import {
+  friendsHeadLine,
+  friendsIntro,
+  projects,
+  friends,
+} from '@/config/infoConfig'
 
 import { FriendCard } from '@/components/friends/FriendCard'
 import { GithubProjectCard } from '@/components/project/GithubProjectCard'
@@ -14,13 +19,10 @@ export const metadata: Metadata = {
 
 export default function Friends() {
   return (
-    <SimpleLayout
-      title={friendsHeadLine}
-      intro={friendsIntro}
-    >
+    <SimpleLayout title={friendsHeadLine} intro={friendsIntro}>
       <ul
         role="list"
-        className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 lg:grid-cols-3 pb-10"
+        className="grid grid-cols-1 gap-x-8 gap-y-12 pb-10 sm:grid-cols-2 lg:grid-cols-3"
       >
         {friends.map((friend) => (
           <FriendCard key={friend.name} friend={friend} />
